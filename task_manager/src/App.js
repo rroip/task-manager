b108e500db432e25eeae44cf6d4da20f3a2ed5f8
Stan Sitnitski <stanley@Stans-MBP.socal.rr.com>
2023-05-16 19:13:31 -0700

    html component

--- task_manager/src/App.js
+++ task_manager/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-
+import TodoList from './TodoList';
 
 function App() {
-  return null
+  return (
+    <>
+    <TodoList />
+    <input type='text' />
+    <button>Add A Task</button>
+    <button>Clear Completed Tasks</button>
+    <div></div>
+    </>
+  )
     
 }
 
